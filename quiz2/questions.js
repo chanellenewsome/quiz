2ae91ed52956dd4179f7c404d030a7bf1f35bd20
chanellenewsome <92777221+chanellenewsome@users.noreply.github.com>
2022-01-30 16:37:09 -0500

    Add files via upload

---
+++ quiz2/questions.js
@@ -0,0 +1,47 @@
+var questions = [
+    { 
+        question: "Commonly used data types DO NOT Include:"       
+         answers: [
+            { text: "1. Strings", correct: false },
+            { text: "2. Bootleans", correct: true },
+            { text: "3. Alerts", correct: false },
+            { text: "4. Numbers", correct: false }
+        ]
+    },
+    { 
+        question: "The condition in an if/else statement is enclosed with ___________", 
+        answers: [
+            { text: "1. Quotes", correct: false },
+            { text: "Curly brackets", correct: true},
+            { text: "Parenthesis", correct: false},
+            { text: "Square brackets", correct: false }
+        ]
+    },
+    { 
+        question: "Arrays in JS can be used to store ______________.", 
+        answers: [
+            { text: "1. Numbers and strings", correct: true },
+            { text: "2. Other arrays", correct: false },
+            { text: "3. Bootleans", correct: false },
+            { text: "4. All of the above", correct: false }
+        ]
+    },
+    { 
+        question: "String values must be enclosed within __________ when being assigned to variables." 
+        answers: [
+            { text: "1. Commas", correct: false },
+            { text: "2. Curly brackets", correct: true },
+            { text: "3. quotes", correct: false },
+            { text: "4. parenthesis", correct: false }
+        ]
+    },
+    { 
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            { text: "1. Javascript", correct: false },
+            { text: "2. Terminal/bash", correct: false },
+            { text: "3. For loops", correct: false },
+            { text: "4. Console log", correct: true }
+        ]
+    },
+];
